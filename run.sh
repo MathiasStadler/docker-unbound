772a5f1205ff47aa93a00032d69284e4a2f45083
MathiasStadler <stadler-mathias@web.de>
2017-08-19 18:08:13 +0200

    save

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-
-
-
-# TODO check container is running
-# TODO ask stop conainer 
+# TODO DONE check container is running
+# TODO DONE would you stop a running container 
 # TODO is port 53 free eg is used from dnsmasq
-
 
 #set env
 GIT_OWNER_NAME=$(git config user.name| tr '[:upper:]' '[:lower:]')
 BASH_OWNER_NAME=$(id -u -n)
-OWNER_NAME=${GIT_OWNER_NAME:-BASH_OWNER_NAME}
+OWNER_NAME=${GIT_OWNER_NAME:-$BASH_OWNER_NAME}
 CONTAINER_NAME=${CONTAINER_NAME:-c-docker-unbound}
 IMAGES_NAME=${IMAGES_NAME:-i-docker-unbound}
 TAG_NAME=${TAG_NAME:-latest}
-
 
 #getopt
 while getopts ":n" opt; do
@@ -31,15 +26,38 @@ while getopts ":n" opt; do
   esac
 done
 
-
 #exit on error
 set -e
 
+showAllUsedPort(){
+
+# from here
+# https://askubuntu.com/questions/699508/how-to-extract-mapped-ports-from-docker-pss-output
+sudo docker ps | awk -F" {2,}" 'END {print $6}'
+
+}
+
+
 checkRunningContainerAndStop() {
-if docker ps |grep -q ${OWNER_NAME}/${IMAGES_NAME}; then
-echo "$(docker ps | grep ${OWNER_NAME}/${IMAGES_NAME}|wc -l ) running Container found ... $(docker ps |grep  ${OWNER_NAME}/${IMAGES_NAME}|awk '{print $1}')"
-echo "stopping container now ..."
-docker ps |grep  ${OWNER_NAME}/${IMAGES_NAME}|awk '{print $1}' | xargs --no-run-if-empty docker stop
+if docker ps |grep -q "${OWNER_NAME}/${IMAGES_NAME}"; then
+#TODO What is if we found more than container ?
+echo "$(docker ps | grep -c "${OWNER_NAME}/${IMAGES_NAME}") running Container found ... $(docker ps |grep  "${OWNER_NAME}/${IMAGES_NAME}"|awk '{print $1}')"
+
+read -r -p "Would you like stop this container now ? [y/N]" response
+      case "$response" in [yY][eE][sS]|[yY]) 
+        echo "stopping container now ..."
+        docker ps |grep  "${OWNER_NAME}/${IMAGES_NAME}"|awk '{print $1}' | xargs --no-run-if-empty docker stop  >/dev/null
+         ;;
+        *)
+          echo "A current conntainer is still running ..."
+          docker ps | grep  "${OWNER_NAME}/${IMAGES_NAME}" | grep "${CONTAINER_NAME}" | grep "${TAG_NAME}"
+          echo "Have fun with it...ciao"
+	        exit 0
+        ;;
+      esac
+
+
+
 else
 echo "No container ${OWNER_NAME}/${IMAGES_NAME} running!...OK"
 fi
@@ -49,16 +67,15 @@ checkImagesAndBuildNewIfNecessary() {
 
   if [ "$CREATE_NEW_IMAGES" = 'yes' ]; then
     #check images is advaible 
-    if  docker images ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} | grep -q ${OWNER_NAME}/${IMAGES_NAME}; then
-      echo "Delete current images  ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} "
-      docker rmi ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} 
-    else 
-      echo "Images ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} no found...OK"
+    if  docker images "${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME}" | grep -q "${OWNER_NAME}/${IMAGES_NAME}"; then
+      echo "Delete current images  ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME}"
+      docker rmi "${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME}" 
+    else "Images ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} no found...OK"
       echo "Nothing to do !...OK"
     fi 
   fi
 
-  if  docker images ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} | grep -q ${OWNER_NAME}/${IMAGES_NAME}; then
+  if  docker images "${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME}" | grep -q "${OWNER_NAME}/${IMAGES_NAME}"; then
     echo "Images ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} exists...OK"
   else
     echo "Images ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} doesn't exist"
@@ -66,7 +83,7 @@ checkImagesAndBuildNewIfNecessary() {
       case "$response" in [yY][eE][sS]|[yY]) 
         echo "start build images ${OWNER_NAME}/${IMAGES_NAME} " 
         echo "docker build --tag ${OWNER_NAME}/${IMAGES_NAME} --file current/Dockerfile ."
-        docker build --tag ${OWNER_NAME}/${IMAGES_NAME} --file current/Dockerfile $(pwd)/current
+        docker build --tag "${OWNER_NAME}/${IMAGES_NAME}" --file current/Dockerfile "$(pwd)"/current
          ;;
         *)
           echo "Without the images can you not start the docker container!"
@@ -78,7 +95,7 @@ checkImagesAndBuildNewIfNecessary() {
 }
 
 
-deleteOldContainerAndDelete() {
+delteOldContainer() {
 
 # check created container and if availble deleted 
 # we want use always new container
@@ -88,11 +105,11 @@ echo "We would start always a new Container"
 echo "Check old container availble .."
 
 
-if docker ps -a| grep ${CONTAINER_NAME}; then
-  echo "$(docker ps -a| grep ${CONTAINER_NAME}|wc -l ) ${CONTAINER_NAME} container found"
+if docker ps -a| grep "${CONTAINER_NAME}"; then
+  echo "$(docker ps -a| grep -c "${CONTAINER_NAME}" ) ${CONTAINER_NAME} container found"
   echo "We delete the container now ... "
-    docker ps -a| grep ${CONTAINER_NAME} | awk '{print $1}' | xargs --no-run-if-empty docker rm
-      if docker ps -a| grep ${CONTAINER_NAME}; then
+    docker ps -a| grep "${CONTAINER_NAME}" | awk '{print $1}' | xargs --no-run-if-empty docker rm
+      if docker ps -a| grep "${CONTAINER_NAME}"; then
         echo " Error we could't delete the container ${CONTAINER_NAME} ...Not OK"
         exit 1;
       else
@@ -108,16 +125,17 @@ runContainer() {
   echo "run container ..."
   echo "Used ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} to start new ${CONTAINER_NAME} container"
   #start container
-    CID=$(docker run --name ${CONTAINER_NAME}  -d -p 53:53/udp \
-    -v $(pwd)/a-records.conf:/opt/unbound/etc/unbound/a-records.conf:ro \
-    -v $(pwd)/root-hints:/opt/unbound/etc/unbound/root.hints:ro \
-    ${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME} )
+    CID=$(docker run --name "${CONTAINER_NAME}"  -d \
+    -p 53:53/udp \
+    -v "$(pwd)"/a-records.conf:/opt/unbound/etc/unbound/a-records.conf:ro \
+    -v "$(pwd)"/root-hints:/opt/unbound/etc/unbound/root.hints:ro \
+    "${OWNER_NAME}/${IMAGES_NAME}:${TAG_NAME}" )
 
     #give docker few seconds
     sleep 2
     
-    if docker ps | grep  ${OWNER_NAME}/${IMAGES_NAME} | grep ${CONTAINER_NAME} | grep -q ${TAG_NAME} ; then 
-      echo "Container ${OWNER_NAME}/${CONTAINER_NAME}:${TAG_NAME} with $(echo ${CID} | head -c 10 ) running...Ok"
+    if docker ps | grep  "${OWNER_NAME}/${IMAGES_NAME}" | grep "${CONTAINER_NAME}" | grep -q "${TAG_NAME}" ; then 
+      echo "Container ${OWNER_NAME}/${CONTAINER_NAME}:${TAG_NAME} with $(echo "${CID}" | head -c 12 ) running...Ok"
     else
       echo "Error container no comming up...Not Ok"
     fi 
@@ -128,7 +146,7 @@ runContainer() {
 stopContainer() {
 echo "stop conatiner ${CONTAINER_NAME}"
 set +e
-sudo docker rm -fv ${CID} >/dev/null 2>&1
+sudo docker rm -fv "${CID}" >/dev/null 2>&1
 set -e
 
 }
@@ -153,7 +171,7 @@ terminated () {
 run(){
 echo "... run "
 checkRunningContainerAndStop
-deleteOldContainerAndDelete
+delteOldContainer
 checkImagesAndBuildNewIfNecessary
 runContainer
 
