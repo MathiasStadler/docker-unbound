20efd2f0681cc234fe6df1a06522a1161ca9e2fc
MathiasStadler <stadler-mathias@web.de>
2017-11-15 20:14:01 +0100

    s

--- restart_unbound_for_test.sh
+++
@@ -1,6 +0,0 @@
-echo "delete all iptables rules"
-echo "resart docker service"
-echo "start unbound"
-./clean-iptables.sh  && \
-service docker restart && \
-./run.sh
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dig +short  @192.168.178.32 mybyod.fritz.box
-dig +short  @192.168.178.32 trapapa-ThinkCentre-M92P.fritz.box
-dig +short  @192.168.178.32 docker-proxy.fritz.box
-
-dig -x +short  @192.168.178.32 192.168.178.21
-dig -x +short  @192.168.178.32 192.168.178.32
-dig -x +short  @192.168.178.32 192.168.178.32
